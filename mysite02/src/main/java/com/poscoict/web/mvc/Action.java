681d20a84161fab6abb06806bc56e7a2a8588b7e
98kimjh <98kimjh@gmail.com>
2022-01-14 17:17:43 +0900

    [mysite02] Using Factory Pattern /w Action&ActionFactory

--- mysite02/src/main/java/com/poscoict/web/mvc/Action.java
+++ mysite02/src/main/java/com/poscoict/web/mvc/Action.java
@@ -7,5 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
-    public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+	// ActionFactory를 통해 결정된 요청의 실제 동작 과정
+	// (Interface로 구현하여 요청마다 다른 동작을 처리하지만 Controller에서는 Action Interface로 묶어서 참조하여 실제 class의 전부를 알 필요 없음 -> 확장성 증대 및 의존성 감소)
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
--- mysite02/src/main/java/com/poscoict/web/mvc/ActionFactory.java
+++ mysite02/src/main/java/com/poscoict/web/mvc/ActionFactory.java
@@ -1,5 +1,6 @@
 package com.poscoict.web.mvc;
 
 public abstract class ActionFactory {
+	// actionName을 통해 client가 어떤 요청을 요구하는지 구분
     public abstract Action getAction(String actionName);
 }
