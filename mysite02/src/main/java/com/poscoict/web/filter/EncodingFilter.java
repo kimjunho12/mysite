f9aa9c9bfa5518326f5003dd87543768b4d07eb2
98kimjh <98kimjh@gmail.com>
2022-01-18 10:52:32 +0900

    [mysite02] Create EncodingFilter

---
+++ mysite02/src/main/java/com/poscoict/web/filter/EncodingFilter.java
@@ -0,0 +1,35 @@
+package com.poscoict.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+	private String encoding;
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		encoding = fConfig.getInitParameter("encoding");
+		if (encoding == null) {
+			encoding = "UTF-8";
+		}
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		/* request 처리 */
+		request.setCharacterEncoding(encoding);
+		
+		chain.doFilter(request, response);
+		
+		/* response 처리 */
+	}
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+}
