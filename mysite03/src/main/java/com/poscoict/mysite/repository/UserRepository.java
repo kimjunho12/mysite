22c647b6bf4df99adf9c0e640f2f5b46d8fe0f47
98kimjh <98kimjh@gmail.com>
2022-01-25 16:11:59 +0900

    [mysite03] Refactor UserRepository(DAO) /w MyBatis

--- mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
@@ -1,216 +1,37 @@
 package com.poscoict.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.mysql.cj.util.StringUtils;
 import com.poscoict.mysite.exception.UserRepositoryException;
 import com.poscoict.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
-	
+
 	@Autowired
-	private DataSource dataSource;
+	private SqlSession sqlSession;
 
 	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException {
-		UserVo result = null;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = dataSource.getConnection();
-
-			// 3. SQL 준비
-			String sql = "SELECT no, name from user WHERE email=? AND password=?";
-			pstmt = conn.prepareStatement(sql);
-
-			// 4. 바인딩
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-
-			// 5. SQL 실행
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-			}
-		} catch (SQLException e) {
-			System.out.println("MYSQL 연결 실패");
-			System.out.print("사유 : " + e.getMessage());
-			throw new UserRepositoryException(e.toString());
-		} finally {
-			// 자원 정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		Map<String, Object> map = new HashMap<>();
+		map.put("email", email);
+		map.put("password", password);
+		return sqlSession.selectOne("user.findByEmailAndPassword", map);
 	}
 
 	public UserVo findByNo(Long no) {
-		UserVo result = null;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = dataSource.getConnection();
-
-			// 3. SQL 준비
-			String sql = "SELECT name, email, gender FROM user WHERE no=?";
-			pstmt = conn.prepareStatement(sql);
-
-			// 4. 바인딩
-			pstmt.setLong(1, no);
-
-			// 5. SQL 실행
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				String name = rs.getString(1);
-				String email = rs.getString(2);
-				String gender = rs.getString(3);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-				result.setEmail(email);
-				result.setGender(gender);
-			}
-		} catch (SQLException e) {
-			System.out.println("MYSQL 연결 실패");
-			System.out.print("사유 : " + e.getMessage());
-		} finally {
-			// 자원 정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return sqlSession.selectOne("user.findByNo", no);
 	}
 
 	public boolean update(UserVo vo) {
-		boolean result = false;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = dataSource.getConnection();
-
-			boolean noPassword = StringUtils.isEmptyOrWhitespaceOnly(vo.getPassword());			
-
-			// 3. SQL 준비
-			String sql = noPassword
-					? "UPDATE user SET name=?, gender=? WHERE no=?"
-					: "UPDATE user SET name=?, gender=?, password=? WHERE no=?";
-			pstmt = conn.prepareStatement(sql);
-
-			// 4. 바인딩
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getGender());
-			if (noPassword) {
-				pstmt.setLong(3, vo.getNo());
-			} else {
-				pstmt.setString(3, vo.getPassword());
-				pstmt.setLong(4, vo.getNo());
-			}
-
-			// 5. SQL 실행
-			result = pstmt.executeUpdate() == 1;
-		} catch (SQLException e) {
-			System.out.println("MYSQL 연결 실패");
-			System.out.print("사유 : " + e.getMessage());
-		} finally {
-			// 자원 정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return 1 == sqlSession.update("user.update", vo);
 	}
 
 	public boolean insert(UserVo vo) {
-		boolean result = false;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = dataSource.getConnection();
-
-			// 3. SQL 준비
-			String sql = "INSERT INTO user VALUES(null, ?, ?, ?, ?, now());";
-			pstmt = conn.prepareStatement(sql);
-
-			// 4. 바인딩
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-
-			// 5. SQL 실행
-			result = pstmt.executeUpdate() == 1;
-		} catch (SQLException e) {
-			System.out.println("MYSQL 연결 실패");
-			System.out.print("사유 : " + e.getMessage());
-		} finally {
-			// 자원 정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return 1 == sqlSession.insert("user.insert", vo);
 	}
 }
