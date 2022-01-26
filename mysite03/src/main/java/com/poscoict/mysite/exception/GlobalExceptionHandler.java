6c6197bdc95d1dde3febb176cb9ba9da51f7b692
98kimjh <98kimjh@gmail.com>
2022-01-26 12:37:39 +0900

    [mysite03] Error Logging /w Logback(SLF4J)

--- mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -3,21 +3,25 @@ package com.poscoict.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
+
 	@ExceptionHandler(Exception.class)
 	public String ExceptionHandler(Model model, Exception e) {
 		// 1. 로깅
 //		String errors = e.toString();
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString());
-				
+		LOGGER.error(errors.toString());
+
 		// 2. 사과 페이지 (HTML 응답, 정상 종료)
 		model.addAttribute("exception", errors.toString());
 		return "error/exception";
