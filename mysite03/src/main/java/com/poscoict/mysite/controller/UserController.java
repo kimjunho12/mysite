d18fa7443ac4af4448b945a9bd901aafd04ab7d0
98kimjh <98kimjh@gmail.com>
2022-01-26 15:27:36 +0900

    [mysite03] Logout /w Interceptor

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -57,13 +57,13 @@ public class UserController {
 //		return "redirect:/";
 //	}
 
-	@RequestMapping(value = "/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("authUser");
-		session.invalidate();
-
-		return "redirect:/";
-	}
+//	@RequestMapping(value = "/logout")
+//	public String logout(HttpSession session) {
+//		session.removeAttribute("authUser");
+//		session.invalidate();
+//
+//		return "redirect:/";
+//	}
 
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update(HttpSession session, Model model) {
