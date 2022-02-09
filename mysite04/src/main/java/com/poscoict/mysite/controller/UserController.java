30a8713c69883764d081df9b51254fd7814c070f
98kimjh <98kimjh@gmail.com>
2022-02-09 11:51:50 +0900

    [mysite04] ApplicationContext config (xml -> java)

--- mysite04/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite04/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.poscoict.mysite.controller;
 
-import java.util.List;
 
 import javax.validation.Valid;
 
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
